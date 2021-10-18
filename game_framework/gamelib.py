afdba5aa2bf7911fdda010d64961f453bb34ee14
Ditthapong lakagul <lakagul.peem@gmail.com>
2021-10-18 15:00:37 +0700

    Create a constant for canvas width, height and delay.

--- game_framework/gamelib.py
+++ game_framework/gamelib.py
@@ -1,6 +1,11 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 
+CANVAS_WIDTH = 800
+CANVAS_HEIGHT = 500
+UPDATE_DELAY = 33
+
+
 class GameCanvasElement():
     """Base class for an element on the game canvas, with attributes:
 
@@ -84,7 +89,7 @@ class GameApp(ttk.Frame):
     on the canvas, start/stop animation, and running the animation loop.
     """
     
-    def __init__(self, parent, canvas_width=800, canvas_height=500, update_delay=33):
+    def __init__(self, parent, canvas_width=CANVAS_WIDTH, canvas_height=CANVAS_HEIGHT, update_delay=UPDATE_DELAY):
         super().__init__(parent)
         self.parent = parent
         
@@ -122,6 +127,15 @@ class GameApp(ttk.Frame):
 
     def start(self):
         self.after(0, self.animate)
+
+    def add_element(self, element: GameCanvasElement):
+        """Add an element to the game."""
+        self.elements.append(element)
+
+    def remove_element(self, element: GameCanvasElement):
+        if element in self.elements:
+            self.elements.remove(element)
+            self.canvas.delete(element.canvas_object_id)
 
     def init_game(self):
         pass
